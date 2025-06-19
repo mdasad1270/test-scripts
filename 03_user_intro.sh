2e6e6addb6e1fb1a823a608560b9765abfa3fda4
mdasad1270 <148677457+mdasad1270@users.noreply.github.com>
2025-06-19 16:52:45 +0530

    Create 03_user_intro.sh

---
+++ 03_user_intro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author Name :- Md Asad
+# Date :- 16.06.2025
+# Task :- Asks the user for: Name, Age and City
+
+read -p "Enter your name :- " name
+read -p "Enter your age :- " age
+read -p "Enter your current city :- " city
+
+echo "Hi, I am $name. I am $age years old and I live in $city."
