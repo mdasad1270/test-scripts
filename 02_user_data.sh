92ef067bea7635144ddf63fd55b7423862eef172
mdasad1270 <148677457+mdasad1270@users.noreply.github.com>
2025-06-19 16:52:13 +0530

    Create 02_user_data.sh

---
+++ 02_user_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Author Name :- Md Asad
+# Date 16.06.2025
+# Task :- Script that reads your name and age, and prints:
+
+read -p "Enter your name :- " name
+read -p "Enter your age :- " age
+
+echo "My name is $name and i am $age years old"
