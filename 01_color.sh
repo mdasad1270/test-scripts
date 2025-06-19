cea77847ccb06d0bed9ab7e295de53b598b6f259
mdasad1270 <148677457+mdasad1270@users.noreply.github.com>
2025-06-19 16:51:39 +0530

    Create 01_color.sh

---
+++ 01_color.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Author Name :- Md Asad
+# Date :- 16.06.2025
+
+read -p "Enter your favorite :- "  color # It will store your favorite color into color variable
+
+echo "My favorite color is $color"     # It will print your color which you stored before in color variable
